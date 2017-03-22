98feab0076730adbb4ad2c7c1eb42b9436963d78
Bhargav Reddi <bhargav.reddi@teradata.com>
2017-03-22 10:24:00 +0530

    Registered Students

---
+++ create_examination_list.py
@@ -0,0 +1,29 @@
+
+
+__author__ = 'Bhargav'
+import django
+import sys,os
+sys.path.append("/home/bhargav/Examcell")
+sys.path.append("/home/mvgrexamcell/Examcell")
+os.environ["DJANGO_SETTINGS_MODULE"] = "Examcell.settings"
+django.setup()
+from Examination.models import Subject,Examination,ExaminationType,Students,RegisteredStudents,Regulation
+subject_list = Subject.objects.all().values_list('subject_code')
+examination = Examination.objects.get(examination_name = '4-2 II Mid Examination 2017')
+regulation = Regulation.objects.get(regulation = 'R13')
+type = ExaminationType.objects.get(type='Mid Term')
+student_list = Student.objects.all().value_list('student_id')
+
+for subject_tuple in subject_list:
+	subject = Subject.objects.get(subject_code = subject_tuple[0])
+	for student_tuple in student_list:
+		student = Student.objects.get(student_id = student_tuple[0])
+		obj = RegisteredStudents()
+		obj.student = student
+		obj.subject = subject
+		obj.regulation = regulation
+		obj.type = type
+		obj.examination = examination
+		obj.save()
+
+
